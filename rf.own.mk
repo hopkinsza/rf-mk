68665289120b0f24e1d869067cf4fb8360acc5a7
Zach Hopkins <hopkinsza@gmail.com>
2022-10-31 22:45:13 -0500

    improve *DIR support, preliminary support for c config file

--- rf.own.mk
+++ rf.own.mk
@@ -8,20 +8,49 @@ _RF_OWN_MK_ = 1
 
 #
 # Directories.
-# Others such as INCDIR and LIBDIR should be defined in their respective files.
 #
 
 PREFIX ?= /usr/local
 
-# installation
-BINDIR ?= $(PREFIX)/bin
+# BIN, INC, LIB, and MAN are traditional.
+BINDIR ?=	$(PREFIX)/bin
+ETCDIR ?=	$(PREFIX)/etc
+INCDIR ?=	$(PREFIX)/include
+LIBDIR ?=	$(PREFIX)/lib
+SHAREDIR ?=	$(PREFIX)/share
+VARDIR ?=	$(PREFIX)/var
 
-ETCDIR ?= $(PREFIX)/etc
-RUNDIR ?= $(VARDIR)/run
-SHAREDIR ?= $(PREFIX)/share
-VARDIR ?= $(PREFIX)/var
+DOCDIR ?=	$(SHAREDIR)/doc
+MANDIR ?=	$(SHAREDIR)/man
+RUNDIR ?=	$(VARDIR)/run
 
-DIRMODE ?= 755
+.if !empty(RF.cconf)
+# XXX: maybe a better way to do this with a .for loop.
+# Need to keep this sync'd with the variables above for now.
+# Also, you need to remove the file (or make clean) to get new values.
+$(RF.cconf):
+	@echo 'create $(.TARGET)'
+	@exec >$(.TARGET); \
+		echo '#ifndef _RF_CONF_H_'; \
+		echo '#define _RF_CONF_H_'; \
+		echo ''; \
+		echo '#define BINDIR	"$(BINDIR)"'; \
+		echo '#define ETCDIR	"$(ETCDIR)"'; \
+		echo '#define INCDIR	"$(INCDIR)"'; \
+		echo '#define LIBDIR	"$(LIBDIR)"'; \
+		echo '#define SHAREDIR	"$(SHAREDIR) "'; \
+		echo '#define VARDIR	"$(VARDIR)"'; \
+		echo '#define DOCDIR	"$(DOCDIR)"'; \
+		echo '#define MANDIR	"$(MANDIR)"'; \
+		echo '#define RUNDIR	"$(RUNDIR)"'; \
+		echo ''; \
+		echo '#endif // _RF_CONF_H_'; \
+
+CLEANFILES := $(CLEANFILES) $(RF.cconf)
+
+realall: $(RF.cconf)
+.ORDER: $(RF.cconf) realall
+.endif
 
 #
 # Default permissions.
@@ -32,6 +61,8 @@ BINGRP ?= wheel
 BINMODE ?= 555
 NONBINMODE ?= 444
 CONFMODE ?= 644
+
+DIRMODE ?= 755
 
 #
 # Derived from netbsd MAKEDIRTARGET as defined in <bsd.own.mk>.
