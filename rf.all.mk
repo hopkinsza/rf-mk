e73f99ff19e764c787c4873be718c6d94bc88ee8
Zach Hopkins <hopkinsza@gmail.com>
2022-11-18 05:35:47 -0600

    add rf.all.mk

---
+++ rf.all.mk
@@ -0,0 +1,17 @@
+#
+# Include rf.init.mk and every rf.* file that would not cause conflicts.
+#
+
+.if !defined(_RF_ALL_MK_)
+_RF_ALL_MK = 1
+
+.include <rf.init.mk>
+
+.include <rf.clean.mk>
+.include <rf.conf.mk>
+.include <rf.config.mk>
+.include <rf.files.mk>
+.include <rf.man.mk>
+.include <rf.subdir.mk>
+
+.endif # _RF_ALL_MK_
